43abdba508cc48d25aa68987e1bcc59f5e873307
Nikos Stanogias <nstanogias@gmail.com>
2019-11-10 21:38:21 +0100

    frontend implementation of dateapp

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-angular-dateapp'`, () => {
+  it(`should have as title 'DatingApp-SPA'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-angular-dateapp');
+    expect(app.title).toEqual('DatingApp-SPA');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-angular-dateapp app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to DatingApp-SPA!');
   });
 });
